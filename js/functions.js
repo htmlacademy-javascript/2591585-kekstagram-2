de358c125b2ba711b918986b0d6553c8e02fc43f
ирина <suntunartist@mail.ru>
2025-11-17 16:40:39 +0300

    creating a function to check the working interval

--- js/functions.js
+++ js/functions.js
@@ -1,53 +1,22 @@
-//Функция 1. Проверка длины строки
-const checkingStringLength = (line = '', maxLength = 1) => line.length <= maxLength;
-
-/* --Тестирование кода--
-console.log(checkingStringLength('проверяемая строка', 20)); // true
-console.log(checkingStringLength('проверяемая строка', 18)); // true
-console.log(checkingStringLength('проверяемая строка', 10)); // false
-*/
-
-//Функция 2. Проверка на палиндром
-const checkPalindrome = (line = '') => {
-  const normalizeLine = line.replaceAll(' ', '').toLowerCase();
-  let invertedLine = '';
-
-  for(let i = normalizeLine.length - 1; i >= 0; i --){
-    invertedLine += normalizeLine[i];
-  }
-
-  return normalizeLine === invertedLine;
+const getTimeInMinutes = (timeString) => {
+  const [hours, minutes] = timeString.split(':').map(Number);
+  return hours * 60 + minutes;
 };
 
-/* --Тестирование кода--
-.log(checkPalindrome('топот')); // true
-console.log(checkPalindrome('ДовОд')); // true
-console.log(checkPalindrome('Кекс')); // false
-console.log(checkPalindrome('Лёша на полке клопа нашёл')); // true
-*/
+const convertTimesToMinutes = (...timeStrings) => timeStrings.map(getTimeInMinutes);
 
-//Функция 3. Дополнительное задание
-const getNumber = (line = '') => {
+const countingWorkingTime = (startWorking, endWorking, startMeeting, durationMeeting) => {
+  const [startWorkingInMinutes, endWorkingInMinutes, startMeetingInMinutes] =
+  convertTimesToMinutes(startWorking, endWorking, startMeeting);
 
-  line = line.toString();
-  let resultingNumber = '';
+  const endMeetingInMinutes = startMeetingInMinutes + durationMeeting;
 
-  for (let i = 0; i < line.length; i ++) {
-    if(!Number.isNaN(parseInt(line[i], 10))) {
-      resultingNumber += line[i];
-    }
-  }
-
-  return resultingNumber === '' ? NaN : Number(resultingNumber);
+  return startMeetingInMinutes >= startWorkingInMinutes && endMeetingInMinutes <= endWorkingInMinutes;
 };
 
-/* --Тестирование кода--
-console.log(getNumber('2023 год')); // 2023
-console.log(getNumber('ECMAScript 2022')); // 2022
-console.log(getNumber('1 кефир, 0.5 батона')); // 105
-console.log(getNumber('агент 007')); // 7
-console.log(getNumber('а я томат')); // NaN
-console.log(getNumber(2023)); // 2023
-console.log(getNumber(-1)); // 1
-console.log(getNumber(1.5)); // 15
-*/
+/*-------------Проверка функции-----------------*/
+//console.log(countingWorkingTime('08:00', '17:30', '14:00', 90)); // true
+//console.log(countingWorkingTime('8:0', '10:0', '8:0', 120)); // true
+//console.log(countingWorkingTime('08:00', '14:30', '14:00', 90)); // false
+//console.log(countingWorkingTime('14:00', '17:30', '08:0', 90)); // false
+//console.log(countingWorkingTime('8:00', '17:30', '08:00', 900)); // false
