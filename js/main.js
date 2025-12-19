8584677f19de2b168b8d4641699c69809bdbc54d
ирина <suntunartist@mail.ru>
2025-12-20 00:14:14 +0300

    implementation of task 12.12 (photo filtering)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 import { renderGallery } from './gallery.js';
 import { initPhotoUploadForm } from './photo-upload-form.js';
 import { getData } from './api.js';
+import { initFilters } from './filters.js';
 import { showDataError } from './messages.js';
 
 getData()
   .then((photos) => {
     renderGallery(photos);
+    initFilters(photos);
   })
   .catch(() => {
     showDataError();
--- js/util.js
+++ js/util.js
@@ -1,3 +1,4 @@
+const TIMEOUT_DELAY = 500;
 const body = document.body;
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
@@ -13,4 +14,12 @@ const showModal = (modal, isShown = true) => {
   body.classList.remove('modal-open');
 };
 
-export { isEscapeKey, showModal};
+const debounce = (callback, timeoutDelay = TIMEOUT_DELAY) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export { isEscapeKey, showModal, debounce};
